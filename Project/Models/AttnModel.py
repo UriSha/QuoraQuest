44a0896e65b1e12625ffa95fde118b92343c51a9
uri <urish92@gmail.com>
2019-01-24 16:10:05 +0200

    Change project structure and gitignore

--- Project/Models/AttnModel.py
+++ Project/Models/AttnModel.py
@@ -1,8 +1,9 @@
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
-import torch.functional as F
-import Attn, MasterClassifier
+
+import Attn
+import MasterClassifier
 
 
 class AttnModel(nn.Module):
