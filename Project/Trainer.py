33190398a4b59c9c72f4eb5fc5b1f53e2c772d10
uri <urish92@gmail.com>
2019-01-24 19:11:57 +0200

    Small change to Trainer

--- Project/Trainer.py
+++ Project/Trainer.py
@@ -12,25 +12,28 @@ class Trainer():
         test_losses = []
 
         for e in range(self.epochs):
-            self.forward_batch(train_X, train_y, train_losses, is_train=True)
-            self.forward_batch(test_X, test_y, test_losses, is_train=False)
+            train_loss = self.forward_batch(train_X, train_y, is_train=True)
+            test_loss = self.forward_batch(test_X, test_y, is_train=False)
 
-            print("epoch: {} | train loss: {} | test_loss: {}".format(e + 1, train_losses[-1], test_losses[-1]))
+            print("epoch: {} | train loss: {} | test_loss: {}".format(e + 1, train_loss, test_loss))
+
+            train_losses.append(train_loss)
+            test_losses.append(test_loss)
 
         return train_losses, test_losses
 
-    def forward_batch(self, X, y, losses_list, is_train):
+    def forward_batch(self, X, y, is_train):
         if is_train:
             self.model.train()
         else:
             self.model.eval()
 
         num_batches = int(len(X) / self.batch_size)
-
         epoch_loss = 0.0
         idx = 0
 
         for j in range(num_batches):
+
             batch_X = X[idx:idx + self.batch_size]
             batch_y = y[idx:idx + self.batch_size]
             idx += self.batch_size
@@ -39,7 +42,6 @@ class Trainer():
                 self.optimizer.zero_grad()
 
             outputs = self.model(batch_X)
-
             loss = self.criterion(outputs, batch_y)
 
             if is_train:
@@ -50,4 +52,4 @@ class Trainer():
 
         # calculate loss for epoch
         epoch_loss /= (num_batches * self.batch_size)
-        losses_list.append(epoch_loss)
+        return epoch_loss
