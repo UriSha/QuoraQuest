f774d7c236daca7bcfd4f849a4a9ed91c493afaa
itay itzhak <Itayit>
2019-01-24 17:01:20 +0200

    clean data

--- Project/DataInit.py
+++ Project/DataInit.py
@@ -1,11 +1,11 @@
 import pandas as pd
-
+import re
 
 class DataInit():
     def __init__(self, file_path):
         data = self.load_data(file_path)
 
-        # data = self.clean_data(data)
+        data = self.clean_data(data)
 
         data = self.preprocess_data(data['question1'], data['question2'])
 
@@ -14,3 +14,13 @@ class DataInit():
 
     def preprocess_data(self, q1, q2):
         pass
+
+    def clean_data(self, data):
+        data['question1'] = data.apply(lambda row: self.normalize_string(row['question1']), axis=1)
+        data['question2'] = data.apply(lambda row: self.normalize_string(row['question2']), axis=1)
+
+    def normalize_string(self, s):
+        s = s.lower().strip()
+        s = re.sub(r"([.!?])", r" \1", s)
+        s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+        return s
