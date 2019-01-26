a790bd4c801b09d25a3a49b6202e390c2d4cf047
uri <urish92@gmail.com>
2019-01-26 11:13:47 +0200

    shuffle data every epoch

--- Project/Trainer.py
+++ Project/Trainer.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Trainer():
     def __init__(self, model, optimizer, criterion, epochs, batch_size):
         self.model = model
@@ -12,6 +15,8 @@ class Trainer():
         test_losses = []
 
         for e in range(self.epochs):
+            # train_X, train_y = self.shuffle_data(train_X, train_y)
+
             train_loss = self.forward_batch(train_X, train_y, is_train=True)
             test_loss = self.forward_batch(test_X, test_y, is_train=False)
 
@@ -34,7 +39,7 @@ class Trainer():
 
         for j in range(num_batches):
 
-            batch_X = X[idx:idx + self.batch_size]
+            batch_X = X[idx:idx + self.batch_size * 2]
             batch_y = y[idx:idx + self.batch_size]
             idx += self.batch_size
 
@@ -53,3 +58,9 @@ class Trainer():
         # calculate loss for epoch
         epoch_loss /= (num_batches * self.batch_size)
         return epoch_loss
+
+    # def shuffle_data(self, X, y, seed=4):
+    #     c = list(zip(X, y))
+    #     random.Random(seed).shuffle(c)
+    #     X, y = zip(*c)
+    #     return X, y
