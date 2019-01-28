ddefa484900a1e384eca405eed89f88331e65b98
Daniel Dubnikov <t-dadub@microsoft.com>
2019-01-28 19:33:42 +0200

    Update AttnModel.py

--- Project/Models/AttnModel.py
+++ Project/Models/AttnModel.py
@@ -7,14 +7,39 @@ from Models.MasterClassifier import MasterClassifier
 
 
 class AttnModel(nn.Module):
-    def __init__(self, question_embed_size):
+    def __init__(self, src_vecs, cuda):
         super(AttnModel, self).__init__()
 
-        self.question_embed_size = question_embed_size
-        self.attn = Attn(question_embed_size)
-        self.cls = MasterClassifier(question_embed_size)
+        self.semb = nn.Embedding(src_vecs.vocab_length, src_vecs.vector_size)
+        self.semb.weight.data.copy_(torch.from_numpy(src_vecs._matrix))
+        self.w2idx = src_vecs._w2idx
+        self.idx2w = src_vecs._idx2w
+        self.emb_size = src_vecs.vector_size
+        self.to_cuda = cuda
+
+        # Do not update original embedding spaces
+        self.semb.weight.requires_grad = False
+
+        self.attn = Attn(self.emb_size)
+        self.cls = MasterClassifier(self.emb_size * 2)
+
+        if self.to_cuda:
+            self.cls.cuda()
+            self.attn.cuda()
 
     def forward(self, X):
+
+        #X = Padder(X)
+
+        X = self.concat_pairs(X)
+
+        X = self.cls(X)
+
+        X = X.squeeze(dim=1)
+
+        return X
+
+
         first_q = X[:self.question_embed_size]
         sec_q = X[self.question_embed_size:]
 
@@ -34,7 +59,7 @@ class AttnModel(nn.Module):
         # get the length of each sentence
         batch_lengths = [len(sentence) for sentence in batch_x]
 
-        batch_x = [self.sent_to_indecies(sent) for sent in batch_x]
+        batch_x = [self.sent_to_indices(sent) for sent in batch_x]
         # create an empty matrix with padding tokens
         #   pad_token = vocab['<PAD>']
         pad_token = 0
@@ -46,9 +71,19 @@ class AttnModel(nn.Module):
             sequence = batch_x[i]
             padded_batch[i, 0:sent_len] = sequence[:sent_len]
 
-        batch = [torch.cuda.LongTensor(l) for l in padded_batch]
+        if self.to_cuda:
+            batch = [torch.LongTensor(l) for l in padded_batch]
+        else:
+            batch = [torch.cuda.LongTensor(l) for l in padded_batch]
         return torch.stack(batch), batch_lengths
 
-    def sent_to_indecies(self, sentence):
+    def sent_to_indices(self, sentence):
         return [self.w2idx(word) for word in sentence]
 
+    def concat_pairs(self, X):
+        """
+        Gets a batch of embedded pairs of questions
+         (a pair of questions is the i'th tensor and (i+1)'th tensor for every even i)
+        @:return Tensor of concatenated pairs of questions
+        """
+        return torch.stack([torch.cat((X[i], X[i + 1]), 0) for i in range(0, len(X), 2)])
